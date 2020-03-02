ea1349b35907741dacd4bb6ac74944a280aa5709
kellyeryan <kellyeryan@gmail.com>
2020-03-02 11:49:20 -0500

    Finish react forms lab

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,27 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
+  }
+
+  handleCharacterChange = event => {
+    this.setState({
+      message: event.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text"
+        name="message"
+        id="message"
+        value={this.state.message}
+        onChange={event=>this.handleCharacterChange(event)} />
+        {this.props.maxChars - this.state.message.length}
       </div>
     );
   }
